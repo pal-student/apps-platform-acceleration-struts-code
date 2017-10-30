b3e90ddf3885485fef431140f87ce942b17883b4
Serhiy Ver <4bugzilla@gmail.com>
2017-10-30 10:33:10 -0600

    struts o spring boot

---
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -0,0 +1,57 @@
+package org.superbiz.struts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class UsersController {
+
+    private UserRepository userService;
+
+    public UsersController(UserRepository userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/addUser")
+    public String addUserForm() {
+        return "addUserForm";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user) {
+
+        userService.save(user);
+
+        return "addedUser";
+    }
+
+    @GetMapping("/findUser")
+    public String findUserForm() {
+        return "findUserForm";
+    }
+
+    @PostMapping("/findUser")
+    public String findUser(@RequestParam long id, Model model) {
+        User user = userService.findOne(id);
+
+        if (user == null) {
+            model.addAttribute("errorMessage", "User not found");
+            return "findUserForm";
+        }
+
+        model.addAttribute("user", user);
+        return "displayUser";
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "displayUsers";
+    }
+}
